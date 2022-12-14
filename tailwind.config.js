e74319f484f25bdc281d5a4b4ef2691288ee794a
Alex <axel-pi@gmx.de>
2022-12-14 14:14:04 +0100

    Change styles with (extended) tailwindcss breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       aspectRatio: {
         '3/2': '3 / 2',
       },
+      screens: {
+        '3xl': '1920px',
+        '4xl': '2560px',
+      },
     },
   },
   plugins: [],
